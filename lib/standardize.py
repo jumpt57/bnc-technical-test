759dcfea92e7e1156f10c47ba0a7731a987d7f28
jumpt57 <julienmaupetit@icloud.com>
2023-01-10 15:20:36 -0500

    - Add get files with regex with tests

--- lib/standardize.py
+++ lib/standardize.py
@@ -1,2 +1,17 @@
-def get_data_files():
-    pass
+import os
+import re
+from os import listdir
+from os.path import isfile, join
+
+
+def get_data_files(regex: str = "") -> list[str]:
+    compiled_regex = re.compile(regex)
+
+    this_dir = os.getcwd()
+    path = join(this_dir, "../dataDropArea")
+
+    return [
+        file
+        for file in listdir(path)
+        if isfile(join(path, file)) and compiled_regex.match(file)
+    ]
--- tests/test_standardize.py
+++ tests/test_standardize.py
@@ -1,7 +1,11 @@
 from standardize import get_data_files
 
 
-def test_loop_through_files_in_data_drop_area_directory():
+def test_loop_through_files_in_data_drop_area_directory_without_regex():
     files = get_data_files()
+    assert len(files) == 6
 
+
+def test_loop_through_files_in_data_drop_area_directory_with_regex():
+    files = get_data_files("file_test_1_[0-9]{8}.csv")
     assert len(files) == 3
