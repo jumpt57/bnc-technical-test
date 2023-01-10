b912e10f1fb42c01d8b1d123ebeb5a8c82addffe
jumpt57 <julienmaupetit@icloud.com>
2023-01-10 16:12:02 -0500

    - Add tests - Add yaml lib - Add func to get conf file

--- lib/standardize.py
+++ lib/standardize.py
@@ -2,6 +2,10 @@ import os
 import re
 from os import listdir
 from os.path import isfile, join
+from pathlib import Path
+from types import NoneType
+
+import yaml
 
 
 def get_data_files(regex: str = "") -> list[str]:
@@ -10,8 +14,20 @@ def get_data_files(regex: str = "") -> list[str]:
     current_dir = os.path.abspath(os.path.dirname(__file__))
     path = join(current_dir, "../dataDropArea")
 
-    return [
+    files = [
         file
         for file in listdir(path)
         if isfile(join(path, file)) and compiled_regex.match(file)
     ]
+
+    if len(files) == 0:
+        raise FileNotFoundError(f"It was not possible to find files with regex {regex}")
+
+    return files
+
+
+def get_conf_file(file_name: str) -> dict:
+    current_dir = os.path.abspath(os.path.dirname(__file__))
+    path = join(current_dir, "../param", file_name)
+
+    return yaml.safe_load(Path(path).read_text())
--- tests/test_standardize.py
+++ tests/test_standardize.py
@@ -1,4 +1,6 @@
-from standardize import get_data_files
+import pytest
+
+from standardize import get_data_files, get_conf_file
 
 
 def test_loop_through_files_in_data_drop_area_directory_without_regex():
@@ -9,3 +11,18 @@ def test_loop_through_files_in_data_drop_area_directory_without_regex():
 def test_loop_through_files_in_data_drop_area_directory_with_regex():
     files = get_data_files("file_test_1_[0-9]{8}.csv")
     assert len(files) == 3
+
+
+def test_error_loop_through_files_in_data_drop_area_directory_with_regex():
+    with pytest.raises(FileNotFoundError):
+        get_data_files("test")
+
+
+def test_read_conf_file():
+    conf = get_conf_file("config.yaml")
+
+    assert conf == {
+        "file_test_1_[0-9]{8}.csv": {"delimiter": ",", "column_count": 5},
+        "file_test_2_[0-9]{8}.csv": {"delimiter": ";", "column_count": 4},
+        "file_test_3_[0-9]{8}.csv": {"delimiter": "|", "column_count": 4},
+    }
